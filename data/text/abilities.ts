f72249eb0e05785dd87a499959726df77132493a
Kriliin <77649528+Kriliin@users.noreply.github.com>
2021-01-19 03:25:19 +0100

    Update abilities.ts

--- data/text/abilities.ts
+++ data/text/abilities.ts
@@ -19,8 +19,8 @@ export const AbilitiesText: {[k: string]: AbilityText} = {
 	},
 	aftermath: {
 		name: "Aftermath",
-		desc: "If this Pokemon is knocked out with a contact move, that move's user loses 1/4 of its maximum HP, rounded down. If any active Pokemon has the Damp Ability, this effect is prevented.",
-		shortDesc: "If this Pokemon is KOed with a contact move, that move's user loses 1/4 its max HP.",
+		desc: "If this Pokeman is knocked out with a contact move, that move's user loses 1/4 of its maximum HP, rounded down. If any active Pokemon has the Damp Ability, this effect is prevented.",
+		shortDesc: "If this Pokeman is KOed with a contact move, that move's user loses 1/4 its max HP.",
 
 		damage: "  [POKEMON] was hurt!",
 	},
